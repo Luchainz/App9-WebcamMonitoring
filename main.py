7d969a9eea9277ce215a7d070399ec25fa0f82fe
Luchainz <djluchainz@gmail.com>
2023-07-21 15:19:52 -0400

    when an image comes into the frame, pictures are taken and put into an "images" directory. After put into the directory, the image in the middle of the list is selected as the "main image".

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import cv2
 import time
 from emailing import send_email
+import glob
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
 
 while True:
     status = 0
     check, frame = video.read()
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21,), 0)
-    cv2.imshow("My video", gray_frame_gau)
 
     if first_frame is None:
         first_frame = gray_frame_gau
@@ -34,12 +36,19 @@ while True:
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) /2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
         send_email()
+
+    print(status_list)
 
     cv2.imshow("Video", frame)
 
